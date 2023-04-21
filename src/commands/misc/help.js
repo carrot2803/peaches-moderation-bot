a6fd8704b8d9371dd15fc1181571925e3140c0be
carrot2803 <lezamadmitri@gmail.com>
2023-04-20 22:34:03 -0400

    dynamically generates command information

---
+++ src/commands/misc/help.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Displays a list of available commands.'),
+    execute(interaction, client) {
+        const commands = client.commands.map(command => command.data.toJSON());
+        const commandList = commands.map(command => `- \`${command.name}\`: ${command.description}`).join('\n');
+
+        const embed = {
+            color: parseInt('0099ff', 16), // convert hex code to integer
+            title: 'Available Commands',
+            description: commandList,
+            footer: {
+                text: "carrot's Bot"
+            }
+        };
+
+        interaction.reply({ embeds: [embed] });
+    },
+};
