c47d2b0123c57078bfadceb985a433a5f2c8ba94
carrot2803 <lezamadmitri@gmail.com>
2023-04-20 22:34:51 -0400

    automatically assigns a role set

---
+++ src/commands/moderation/auto-role.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+let roleName = '';
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('auto-role')
+        .setDescription('Set a role for auto-role')
+        .addRoleOption(option => option.setName('role').setDescription('Role to set for auto-role')),
+    async execute(interaction) {
+        const role = interaction.options.getRole('role');
+        roleName = role ? role.name : '';
+        await interaction.reply({ content: `Auto-role set to ${roleName}`, ephemeral: false });
+    },
+    getroleName() {
+        return roleName;
+    }
+};
